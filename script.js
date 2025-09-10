08418889fc1a810da1cee350b90c5bd100133800
v1nixx <vinicius_santos15@estudante.sesisenai.org.br>
2025-09-10 19:40:45 -0300

    Add README and initial JavaScript implementation

---
+++ script.js
@@ -0,0 +1,30 @@
+// portugol para JavaScript
+//declaração de variáveis
+
+/*em portugol
+cadeia nome = "João"
+cadeia sobrenome = "Silva"
+inteiro idade = 25
+real altura = 1.75
+logico ehHumano = verdadeiro
+escreva("Nome: ", nome)
+escreva("Sobrenome: ", sobrenome)   gordo vinicius
+*/
+
+let nome
+let sobrenome
+let idade
+let altura
+let ehHumano
+
+nome = "João"
+sobrenome = "Silva"
+idade = 25
+altura = 1.75
+ehHumano = true
+
+console.log("Nome:", nome)
+console.log("Sobrenome:", sobrenome)
+console.log("Idade:", idade)
+console.log("Altura:", altura)
+console.log("É humano?", ehHumano)
