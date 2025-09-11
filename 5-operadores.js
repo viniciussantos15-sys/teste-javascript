e97d98b798776659006aeb6dc5e87dbb9866b118
viniciussantos15-sys <vinicius_santos15@estudante.sesisenai.org.br>
2025-09-11 18:12:38 -0300

    Update 5-operadores.js

---
+++ 5-operadores.js
@@ -0,0 +1,12 @@
+// igualdade (==, ===) e diferença (!=, !==)
+console.log(5 = '5') // true (valor)
+
+console.log(5 === '5') // false (valor e tipo)
+
+console.log(5 != '5') // false (valor)
+
+console.log(5 !== '5') // true (valor)
+
+console.log(5 > 3) // true (valor e tipo)
+
+console.log(10 < 10) // false (valor e tipo)
